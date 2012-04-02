2eb901a36ea661b31a8e4d054651774479e3af1c
thomas plotkowiak <ubuntu@ip-10-28-135-8.ec2.internal>
2012-04-02 07:32:44 +0000

    yahoo scrape

--- analysis/yahoo/scrape_yahoo.rb
+++ analysis/yahoo/scrape_yahoo.rb
@@ -3,33 +3,56 @@ require 'nokogiri'
 require 'open-uri'
 
 
-@file = File.open("yahoo.csv", "w+")
+#topdomains = ["arts", "news_and_media","business_and_economy", "recreation", "computers_and_internet", "reference", "education", "regional", "entertainment", "science", "government", "social_science", "health", "society_and_culture"]
+topdomains = ["business_and_economy", "recreation", "computers_and_internet", "reference", "education", "regional", "entertainment", "science", "government", "social_science", "health", "society_and_culture"]
 
-topdomains = ["arts", "news_and_media","business_and_economy", "recreation", "computers_and_internet", "reference", "education", "regional", "entertainment", "science", "government", "social_science", "health", "society_and_culture"]
+@seen_words = []
+File.readlines("seenwords.csv").each do |line|
+        @seen_words << line.sub!(/\n/,"")
+end
+@seen_words_file = File.open("seenwords.csv", "a+")
 
-seen_words = []
+
+def write_net(father, son)
+	 if @seen_words.include?(son) && !son.include?("@")
+                        @file.puts "#{father} #{father}_#{son}"
+         else
+                        @file.puts "#{father} #{son}"
+         end
+         @seen_words_file.puts son
+end
+
+i = 0
 topdomains.each do |domain|
-
+	@file = File.open("#{domain}.csv", "w+")
+	puts "done domain #{domain}"
 	site =  Nokogiri::HTML(open("http://dir.yahoo.com/#{domain}"))
 	site.css("div.cat li a").each do |link|
-		short_link = link.content.gsub(" ","_").downcase
-		if seen_words.include?(short_link) and !short_link.include?("@")
-    		  	@file.puts "#{domain} #{domain}_#{short_link}"
+		first_level_link = link.content.gsub(" ","_").downcase
+		write_net(domain,first_level_link)
+		puts "working on #{first_level_link}"
+		if first_level_link.include? "@"
+			first_level_link.gsub!("@","")
+			sub_site = Nokogiri::HTML(open("http://dir.yahoo.com/#{first_level_link}"))
 		else
-			@file.puts "#{domain} #{short_link}"
+			sub_site = Nokogiri::HTML(open("http://dir.yahoo.com/#{domain}/#{first_level_link}"))
 		end
-		seen_words << short_link
-		sub_site = Nokogiri::HTML(open("http://dir.yahoo.com/#{domain}/#{short_link}"))
 		sub_site.css("div.cat li a").each do |sub_link|
-			sub_short_link = sub_link.content.gsub(" ","_").downcase
-			if seen_words.include? sub_short_link and !sub_short_link.include? "@"
-                	        @file.puts "#{short_link} #{short_link}_#{sub_short_link}"
-	                else
-        	                @file.puts "#{short_link} #{sub_short_link}"
-                	end
-			seen_words << sub_short_link
+			i += 1
+			puts i.to_s
+			second_level_link = sub_link.content.gsub(" ","_").downcase
+			write_net(first_level_link, second_level_link)
+			if second_level_link.include? "@"
+				second_level_link.gsub!("@","")
+				sub_sub_site = Nokogiri::HTML(open("http://dir.yahoo.com/#{second_level_link}"))
+			else
+				sub_sub_site = Nokogiri::HTML(open("http://dir.yahoo.com/#{domain}/#{first_level_link}/#{second_level_link}"))
+			end
+			sub_sub_site.css("div.cat li a").each do |sub_sub_link|
+				third_level_link = sub_sub_link.content.gsub(" ","_").downcase
+				write_net(second_level_link, third_level_link)
+			end
 		end
 	end
 end
 @file.close
-
