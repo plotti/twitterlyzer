aa4578b753fb4ee27df1d4961e855bede5ce67e0
thomas plotkowiak <ubuntu@ip-10-28-135-4.ec2.internal>
2012-05-15 14:23:32 +0000

    added group blockmodelling routine

--- analysis/group_bridging.py
+++ analysis/group_bridging.py
@@ -1,4 +1,17 @@
 import networkx as nx
+import csv
 
-for project in project_pairs:
+csv_writer = csv.writer(open('results/group_bridging.csv', 'wb'))
+
+project = 4
+
     
+FF = nx.read_edgelist('data/%s_FF.edgelist' % project, nodetype=str, data=(('weight',float),),create_using=nx.DiGraph())
+AT = nx.read_edgelist('data/%s_AT.edgelist' % project, nodetype=str, data=(('weight',float),),create_using=nx.DiGraph())
+RT = nx.read_edgelist('data/%s_RT.edgelist' % project, nodetype=str, data=(('weight',float),),create_using=nx.DiGraph())
+
+
+partition = [FF.nodes()[0:10],FF.nodes()[10:101]]
+M = nx.blockmodel(FF,partition)
+
+nx.write_pajek(M,"%s.net" % project)
