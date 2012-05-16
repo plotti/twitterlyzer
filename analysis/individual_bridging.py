ad11e6e0c0530b41c77c0eb922a66885b6fb0ec2
thomas plotkowiak <ubuntu@ip-10-28-135-4.ec2.internal>
2012-05-16 14:00:17 +0000

    addede

--- analysis/individual_bridging.py
+++ analysis/individual_bridging.py
@@ -32,9 +32,11 @@ for key, group in groupby(temp, lambda x: x[1]):
         partition.append(g)
 
 #Genereate a subgraph consisting out of two partitions
+#With n= 2(pairs of 2)  and k = 200 (~number of groups) we can generate 19900 combinations
 p1 = 100
 p2 = 12
 S_FF = FF.subgraph(partition[p1]+partition[p2])
+
 
 #Relabel for pajek
 def mapping(x):
@@ -44,3 +46,10 @@ H_FF = nx.relabel_nodes(S_FF,mapping)
 
 #Write it to disk
 nx.write_pajek(H_FF,"results/networks/%s_%s_%s_pair_FF.net" % (project, groups[p1], groups[p2]))
+
+#TODO: Calculate betweeness for actors 
+#TODO: Correlate betweeness against information
+#a) received from own group
+#b) received from other group
+#c) sent to own group 
+#d) sent to other group
