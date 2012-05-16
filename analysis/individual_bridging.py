c1315390a2307e16d75aae8df58eed0f2d57240a
thomas plotkowiak <ubuntu@ip-10-28-135-4.ec2.internal>
2012-05-16 12:44:55 +0000

    added individual bridging function

---
+++ analysis/individual_bridging.py
@@ -0,0 +1,46 @@
+import networkx as nx
+import csv
+from itertools import groupby
+
+def uniq(seq):
+   # order preserving
+   checked = []
+   for e in seq:
+       if e not in checked:
+           checked.append(e)
+   return checked
+
+# Get the overall network from disk
+project = "all"
+FF = nx.read_edgelist('data/%s_FF.edgelist' % project, nodetype=str, data=(('weight',float),),create_using=nx.DiGraph())
+
+#Get the partition from a partition file
+reader = csv.reader(open("data/partitions.csv", "rb"), delimiter=",")
+temp  = []
+partition = []
+for row in reader:
+        temp.append([row[0],row[1]])
+
+#Sort the list according to category
+temp = sorted(temp, key=lambda x: x[1])
+groups = uniq(map(lambda x: x[1],temp))
+
+for key, group in groupby(temp, lambda x: x[1]):
+        g = []
+        for thing in group:
+                g.append(thing[0])
+        partition.append(g)
+
+#Genereate a subgraph consisting out of two partitions
+p1 = 100
+p2 = 12
+S_FF = FF.subgraph(partition[p1]+partition[p2])
+
+#Relabel for pajek
+def mapping(x):
+	return "\"%s\"" % x
+
+H_FF = nx.relabel_nodes(S_FF,mapping)
+
+#Write it to disk
+nx.write_pajek(H_FF,"results/networks/%s_%s_%s_pair_FF.net" % (project, groups[p1], groups[p2]))
