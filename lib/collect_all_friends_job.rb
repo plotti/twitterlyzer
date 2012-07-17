26d0d75aa1e4c6897e4a10bd96ca190daff3f530
Thomas Plotkowiak <plotti@gmx.net>
2012-07-17 17:50:03 +0200

    big commit after major changes

--- lib/collect_all_friends_job.rb
+++
@@ -1,7 +0,0 @@
-class CollectAllFriendsJob < Struct.new(:id, :project_id)
-  def perform
-    person = Person.find(id)    
-    print "Collecting " + person.friends_count.to_s + " friends of: " +  person.username 
-    Person.collect_all_friends(person,project_id)
-  end  
-end
